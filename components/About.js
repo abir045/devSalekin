1ecc066943f0ca9730dd993452f5c2ec561f83ec
Salekin Imran <abir045@gmail.com>
2022-05-26 19:12:47 +0600

    added container in index components

--- components/About.js
+++ components/About.js
@@ -7,9 +7,9 @@ import dev from "../public/undraw.png";
 
 const About = () => {
   return (
-    <div className="container  mx-auto">
+    <div className="container mx-auto">
       <div className="lg:space-x-5 lg:flex lg:flex-row item-center lg:-mx-4 flex flex-col-reverse text-center lg:text-left">
-        <div className="lg:px-4 lg:mt-12 ">
+        <div className=" lg:px-4 lg:mt-12 ">
           <h1 className="text-2xl font-bold text-gray-900 lg:text-5xl dark:text-white">
             Front End Developer
           </h1>
